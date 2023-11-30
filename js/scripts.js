9b39ee5c24e8222f32d5caddeeff1f0b928f92e4
GiuseppeF11 <faillagiuseppe13@gmail.com>
2023-11-30 16:38:06 +0100

    Aggiunte decorazioni

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,14 @@ const numeroClasse = prompt('Inserisci il numero della tua classe');
 
 console.log(numeroClasse);
 
-const myVar = document.getElementById('testo');
+/* La variabile recupera i tag del HTML tramite l'id 
+e ne SOVRASCRIVE il contenuto tramite inner.HTML*/
 
-let myName = document.getElementById('nome+cognome').innerHTML = 'Benvenuto '+ nome + cognome;
+let myName = document.getElementById('nomecognome').innerHTML = 'Benvenuto '+ nome + cognome;
 
 let myColor = document.getElementById('colore').innerHTML = 'che ama il ' + colorePreferito;
 
 let myClass = document.getElementById('classe').innerHTML = 'della classe #' + numeroClasse + ' sempre al top';
+
+const myVar = document.getElementById('testo');
 
